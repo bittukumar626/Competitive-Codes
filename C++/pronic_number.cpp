f4cb0e7f1ef5a9412d821f078609a716521ea186
Avinash Kumar <115426282+bittukumar626@users.noreply.github.com>
2022-10-11 01:21:03 +0530

    Added Details of Pronic Number

--- C++/pronic_number.cpp
+++ C++/pronic_number.cpp
@@ -1,5 +1,5 @@
+//Pronic number is a number which is the product of two consecutive integers, that is, a number n is a product of x and (x+1)
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
